4e8247a046b8eb607166980d26e7ceac1d244ae5
Ray Tien <a0939552854@gmail.com>
2021-01-20 21:58:57 +0800

    Add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# git-filter-branch
+
+- This script will rewrite all commits author's information, and **_force_** pushing to remote master branch. (Be careful to use this script if you collaborate with other programmer.)
---
+++ git-filter-branch.sh
@@ -0,0 +1,18 @@
+name=$1;
+email=$2;
+if [ "$#" -ne 2 ]; then
+    echo -e "\n"
+    echo -e "[Usuage]: ./git-filter-branch.sh '[author name]' '[author email]'"
+    echo -e "\tDon't forget to place single quote in author name and author email prefix and suffix.\n"
+    exit 0
+else
+    git config user.name "$name";
+    git config user.email "$email";
+    git filter-branch -f --env-filter "
+        GIT_AUTHOR_NAME='$name'
+        GIT_AUTHOR_EMAIL='$email'
+        GIT_COMMITTER_NAME='$name'
+        GIT_COMMITTER_EMAIL='$email'
+    " HEAD
+    git push origin master -f
+fi
